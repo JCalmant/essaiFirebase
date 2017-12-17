1550a272c102df3d35d7a1bc98c63fe0ad50216a
JCalmant <julien.calmant@gmail.com>
2017-12-17 12:24:45 +0100

    user credentials not submitted

---
+++ ChatFirebase2/app/src/main/java/com/henallux/chatfirebase2/UserDetails.java
@@ -0,0 +1,11 @@
+package com.henallux.chatfirebase2;
+
+/**
+ * Created by Julien on 16/12/2017.
+ */
+
+public class UserDetails {
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+}
---
+++ ChatFirebase2/app/src/main/java/com/henallux/chatfirebase2/Users.java
@@ -0,0 +1,8 @@
+package com.henallux.chatfirebase2;
+
+/**
+ * Created by Julien on 16/12/2017.
+ */
+
+public class Users {
+}
